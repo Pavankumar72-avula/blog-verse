910873121482895715a57429d9ba7ffaf0b702ee
PavanKumar72-avula <pavanavula2005@gmail.com>
2025-09-06 15:05:26 +0530

    controlflow

---
+++ controlflow.js
@@ -0,0 +1,29 @@
+let marks = 85;
+if (marks >= 90) {
+    console.log("A+ grade");
+}
+else if (marks >= 80) {
+    console.log("A grade");
+}
+else if (marks >= 70) {
+    console.log("B+ grade");
+}
+else {
+    console.log("fail");
+
+}
+let day =3;
+switch(day){
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;  
+    case 3:
+        console.log("wednesday");
+        break;  
+
+    default:
+        console.log("invalid day");     
+}
