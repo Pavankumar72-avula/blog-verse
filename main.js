55774bc9436da3ca11e7a2a8794f757ada6b8abd
PavanKumar72-avula <pavanavula2005@gmail.com>
2025-09-06 17:10:06 +0530

    responses

---
+++ main.js
@@ -0,0 +1,6 @@
+import{add,multiply,subtract} from "./math.js";
+import divide from "./math.js";
+console.log(add(43, 56))
+console.log(multiply(4,7))
+console.log(subtract(76,15))
+console.log(divide(56,7))
---
+++ math.js
@@ -0,0 +1,18 @@
+export const multiply = function (x, y) {
+    return x * y;
+}
+
+export const add=function(a, b) {
+    return a + b;
+}   
+
+
+export const subtract= (x,y)=> {
+    return x-y;
+}
+const divide = (x, y) => {
+    return x / y;
+}
+export default divide;
+
+    
