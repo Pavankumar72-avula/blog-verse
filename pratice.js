ed716eae31c41745536855aae4b6ba8d0ef5a75f
PavanKumar72-avula <pavanavula2005@gmail.com>
2025-09-06 12:33:42 +0530

    javascript data files

--- pratice.js
+++ pratice.js
@@ -1,3 +1,30 @@
 console.log("this is sample file")
 let name = "pavan";
 console.log(name);
+let num=15;
+console.log(num);
+let islogged=false;
+console.log(islogged);
+let x;
+console.log(x);
+
+let y=null;
+console.log(y);
+
+console.log(typeof num);
+console.log(typeof name);
+console.log(typeof islogged);
+console.log(typeof x);
+console.log(typeof y);
+
+let arr=[1,2,3,4,5];
+console.log(arr[0]);
+console.log(typeof arr);
+
+let person={
+    name:"pavan",
+    age:19
+}
+console.log(person.name);
+console.log(person.age);
+console.log(typeof person);
