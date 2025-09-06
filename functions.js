cee02ff8d382f6c4948f9b135e513a11d00f3c08
PavanKumar72-avula <pavanavula2005@gmail.com>
2025-09-06 15:57:06 +0530

    function

---
+++ functions.js
@@ -0,0 +1,26 @@
+function greet() {
+    console.log("jntugv")
+}
+greet()
+
+function add(a, b) {
+    return a / b;
+}   let result = add(43, 56);
+console.log(result);
+
+const pi=3.14;
+console.log(pi);
+const multiply = function (x, y) {
+    return x * y;
+}
+console.log(multiply(5,7))
+//arrow function
+const square =(x) => x*x;
+console.log(square(6));
+const subtract= (x,y)=> {
+    console.log(x,y);
+    return x-y;}
+    console.log(15,76)
+    console.log(subtract(15,76));
+
+    
