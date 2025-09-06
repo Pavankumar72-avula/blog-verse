7995973661eea1cdda097ef9b4b0354d154e3123
PavanKumar72-avula <pavanavula2005@gmail.com>
2025-09-06 17:10:46 +0530

    responses

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,9 +1,9 @@
-const sampleData=async()=>{
-    try{
-        let response=await fetch("https://www.google.com");
+const sampleData = async () => {
+    try {
+        let response = await fetch("https://www.google.com");
         console.log(response);
-    }catch(err){
-        console.log("Error accessing google:"err);
+    } catch (err) {
+        console.log("Error accessing google:" + err);
     }
 }
 sampleData()
